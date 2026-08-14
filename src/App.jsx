79010566a438fee9beada0fb679f9d2ca7b520f8
Gabriel-Vignoli <pessoalvignoli123@gmail.com>
2026-08-14 16:52:34 -0300

    feat: add Welcome component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import Navbar from "./components/navbar";
+import Welcome from "./components/welcome";
 
 const App = () => {
   return ( 
     <main>
       <Navbar></Navbar>
+      <Welcome></Welcome>
     </main>
    );
 }
